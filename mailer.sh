aa42231599289c374018d224fd0d76a7fc1788fa
Student User <student@workstation.lab.example.com>
2022-10-25 14:21:27 -0400

    prueba4

--- mailer.sh
+++ mailer.sh
@@ -1,7 +1,7 @@
-echo "<!DOCTYPE html>" > /var/www/html/index.html
-echo "<html>" >> /var/www/html/index.html
+sudo echo "<!DOCTYPE html>" > /var/www/html/index.html
+sudo echo "<html>" >> /var/www/html/index.html
 
-echo "<h1>Se cambio el archivo</h1>" >> /var/www/html/index.html
+sudo echo "<h1>Se cambio el archivo</h1>" >> /var/www/html/index.html
 
-echo "</body>" >> /var/www/html/index.html
-echo "</html>" >> /var/www/html/index.html
+sudo echo "</body>" >> /var/www/html/index.html
+sudo echo "</html>" >> /var/www/html/index.html
