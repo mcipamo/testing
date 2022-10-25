0453a200cfebc2b9db906e792a5db5303342af08
Student User <student@workstation.lab.example.com>
2022-10-25 14:35:55 -0400

    prueba7

--- mailer.sh
+++ mailer.sh
@@ -1,2 +1,4 @@
 
-sed -i 's/Este es un ejercicio del examen EX288/Cambiando el index - jueputa/g' /var/www/html/index.html
+#sed -i 's/Este es un ejercicio del examen EX288/Cambiando el index - jueputa/g' /var/www/html/index.html
+
+echo "Lo logramos" >> /var/www/html/index.html
